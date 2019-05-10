e16a7000e19beeee22597b64daf6dc48fd3140d7
temnyles <denishem.shemonaev@gmail.com>
2019-05-10 11:25:01 +0200

    Remise en forme du fichier vecfiles

--- src/vecfiles.cpp
+++ src/vecfiles.cpp
@@ -3,78 +3,73 @@
 Color convert_color(string color)
 {
 	unsigned int i;
-	for(i=0;i<Col_str.size();i++)
+	for (i = 0; i < Col_str.size(); i++)
 	{
-		if(color.compare(Col_str[i])==0)
+		if (color.compare(Col_str[i]) == 0)
 		{
 			return (Color)i;
 		}
 	}
-	cout<<"Couleur non définie"<<endl<<"Par défaut rouge"<<endl;
+	cout << "Couleur non définie" << endl
+		 << "Par défaut rouge" << endl;
 	return red;
-
 }
-
 
 bool is_comment(string lines)
 {
-	if(lines[0] == '#' && lines[1] == '#')
-		{
-			return true;
-		} 
+	if (lines[0] == '#' && lines[1] == '#')
+	{
+		return true;
+	}
 	return false;
 }
 
-Cercle* convert_circle(string str_cir)
+Cercle *convert_circle(string str_cir)
 {
-	int x,y,r,transparence;
+	int x, y, r, transparence;
 	char str[20];
 	Cercle *c = new Cercle();
-	sscanf(str_cir.c_str(),"[CERCLE : %d, %d, %d, %[^,], %d;]",&x,&y,&r,str,&transparence);
-	c->setPosition(x,y);
+	sscanf(str_cir.c_str(), "[CERCLE : %d, %d, %d, %[^,], %d;]", &x, &y, &r, str, &transparence);
+	c->setPosition(x, y);
 	c->setRadius(r);
 	c->setColor(convert_color((string)str));
 	return c;
 }
 
-void openfile(string filename,CImage *img)
+void openfile(string filename, CImage *img)
 {
 	Cercle *c = new Cercle();
 	ifstream vecfile;
 	string lines;
-	vecfile.open(filename.c_str(), std::ifstream::in);   
-	if(!vecfile.is_open())
+	vecfile.open(filename.c_str(), std::ifstream::in);
+	if (!vecfile.is_open())
 	{
 		cout << "Not open" << endl;
 	}
-	while(!vecfile.eof())
+	while (!vecfile.eof())
 	{
-		getline(vecfile,lines);
-		if(!is_comment(lines))
-		{            
-		if((lines.find("CERCLE")!=lines.npos))
+		getline(vecfile, lines);
+		if (!is_comment(lines))
 		{
-			if((lines.find("CERCLE")!=lines.npos))		
-			c = convert_circle(lines);
-			c->drawCircle(img);     
+			if ((lines.find("CERCLE") != lines.npos))
+			{
+				if ((lines.find("CERCLE") != lines.npos))
+					c = convert_circle(lines);
+				c->drawCircle(img);
+			}
+			else if ((lines.find("LIGNE") != lines.npos))
+			{
+			}
+			else if ((lines.find("RECTANGLE") != lines.npos))
+			{
+			}
+			else if ((lines.find("POINT") != lines.npos))
+			{
+			}
+			else if ((lines.find("CARRE") != lines.npos))
+			{
+			}
 		}
-		else if((lines.find("LIGNE")!=lines.npos))
-		{
-			
-		}
-		else if((lines.find("RECTANGLE")!=lines.npos))
-		{
-			
-		}
-		else if((lines.find("POINT")!=lines.npos))
-		{
-			
-		}
-		else if((lines.find("CARRE")!=lines.npos))
-		{
-			
-		}
-		}        
 	}
 	vecfile.close();
 }
